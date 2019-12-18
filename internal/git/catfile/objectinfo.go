dea605a1867c0900aa2e03b371bdf7c2abfc38db
jramsay <pstrokov@gitlab.com>
2019-12-18 15:24:07 +0200

    Improvements to parsing of object information
    Implementation changed to use slice of bytes instead of strings,
    it could improve memory usage and speed up batch operations.
    
    Part of: https://gitlab.com/gitlab-org/gitaly/issues/2255

--- internal/git/catfile/objectinfo.go
+++ internal/git/catfile/objectinfo.go
@@ -2,9 +2,9 @@ package catfile
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"strconv"
-	"strings"
 )
 
 // ObjectInfo represents a header returned by `git cat-file --batch`
@@ -30,29 +30,29 @@ func (o *ObjectInfo) IsBlob() bool {
 
 // ParseObjectInfo reads from a reader and parses the data into an ObjectInfo struct
 func ParseObjectInfo(stdout *bufio.Reader) (*ObjectInfo, error) {
-	infoLine, err := stdout.ReadString('\n')
+	infoLine, err := stdout.ReadSlice('\n')
 	if err != nil {
 		return nil, fmt.Errorf("read info line: %v", err)
 	}
 
-	infoLine = strings.TrimSuffix(infoLine, "\n")
-	if strings.HasSuffix(infoLine, " missing") {
+	infoLine = bytes.TrimSuffix(infoLine, []byte{'\n'})
+	if bytes.HasSuffix(infoLine, []byte(" missing")) {
 		return nil, NotFoundError{fmt.Errorf("object not found")}
 	}
 
-	info := strings.Split(infoLine, " ")
+	info := bytes.Split(infoLine, []byte{' '})
 	if len(info) != 3 {
 		return nil, fmt.Errorf("invalid info line: %q", infoLine)
 	}
 
-	objectSize, err := strconv.ParseInt(info[2], 10, 64)
+	objectSize, err := strconv.ParseInt(string(info[2]), 10, 64)
 	if err != nil {
 		return nil, fmt.Errorf("parse object size: %v", err)
 	}
 
 	return &ObjectInfo{
-		Oid:  info[0],
-		Type: info[1],
+		Oid:  string(info[0]),
+		Type: string(info[1]),
 		Size: objectSize,
 	}, nil
 }
